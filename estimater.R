96232651e85c239e4bab25a587e5be352f7638d2
Tomo-Aot <vespaanalis833@gmail.com>
2025-11-15 13:00:58 +0900

    Add files via upload

--- estimater.R
+++ estimater.R
@@ -68,3 +68,16 @@ pixels |>
   coord_fixed() + 
   theme_void()
 
+
+
+pixels |> 
+  mutate(
+    is_redbrown = 
+      (red >= 30 & red <= 170) &
+      (green >= 5 & green <= 90)  &
+      (blue >= 0 & blue <= 80)
+  ) |> 
+  mutate(colour = ifelse(is_redbrown == TRUE, "#ff0000", colour)) |> 
+  filter(is_redbrown == TRUE) |> 
+  nrow()
+
