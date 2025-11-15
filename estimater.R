896baebcf4a4bd964a4d7eceb1c274294325627f
Tomo-Aot <vespaanalis833@gmail.com>
2025-11-15 13:43:29 +0900

    Update estimater.R

--- estimater.R
+++ estimater.R
@@ -47,14 +47,16 @@ pixels = pixels |>
 
 
 
-pixels |> 
+king = pixels |> 
   mutate(
     is_redbrown = 
       (red >= 30 & red <= 170) &
       (green >= 5 & green <= 90)  &
       (blue >= 0 & blue <= 80)
   ) |> 
-  mutate(colour = ifelse(is_redbrown == TRUE, "#ff0000", colour)) |> 
+  mutate(colour = ifelse(is_redbrown == TRUE, "#ff0000", colour))
+
+king |> 
   ggplot() + 
   geom_raster(
     aes(x = x, y = y, fill = colour)
@@ -69,15 +71,8 @@ pixels |>
   theme_void()
 
 
-
-pixels |> 
-  mutate(
-    is_redbrown = 
-      (red >= 30 & red <= 170) &
-      (green >= 5 & green <= 90)  &
-      (blue >= 0 & blue <= 80)
-  ) |> 
-  mutate(colour = ifelse(is_redbrown == TRUE, "#ff0000", colour)) |> 
+king |> 
   filter(is_redbrown == TRUE) |> 
   nrow()
+
 
